2561ea517a4b5e9000ff02d3b4721ddb92953001
RenanNascimento <renanmateusbn@gmail.com>
2018-04-18 20:46:53 +0000

    paragrafo expandido

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,41 @@
-// Faça o exercício dos parágrafos aqui
-// Este arquivo ainda não está incluído no arquivo HTML
+/* paragrafo de onde eles vem */
+let botaoOndeVem = document.querySelector('#onde-vem');
+
+botaoOndeVem.addEventListener('click', function(){
+    let paragrafo = botaoOndeVem.parentNode;
+    if(botaoOndeVem.textContent === "+"){
+        botaoOndeVem.textContent = "-";
+        paragrafo.classList.add('expandido');
+    }else{
+        botaoOndeVem.textContent = "+";
+        paragrafo.classList.remove('expandido');
+    }
+});
+
+/* paragrafo o que eles querem */
+let botaoOQueQuerem = document.querySelector('#o-que-querem');
+
+botaoOQueQuerem.addEventListener('click', function(){
+    let paragrafo = botaoOQueQuerem.parentNode;
+    if(botaoOQueQuerem.textContent === "+"){
+        botaoOQueQuerem.textContent = "-";
+        paragrafo.classList.add('expandido');
+    }else{
+        botaoOQueQuerem.textContent = "+";
+        paragrafo.classList.remove('expandido');
+    } 
+});
+
+/* paragrafo para onde eles vao */
+let botaoOndeVao = document.querySelector('#onde-vao');
+
+botaoOndeVao.addEventListener('click', function(){
+    let paragrafo = botaoOndeVao.parentNode;
+    if(botaoOndeVao.textContent === "+"){
+        botaoOndeVao.textContent = "-";
+        paragrafo.classList.add('expandido');
+    }else{
+        botaoOndeVao.textContent = "+";
+        paragrafo.classList.remove('expandido');
+    }
+});
